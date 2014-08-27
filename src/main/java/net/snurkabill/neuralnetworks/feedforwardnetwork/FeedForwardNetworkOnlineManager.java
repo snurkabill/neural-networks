a5a07f63be026a48cec900435da727e28fb43bb8
snurakbill <snurkabill@gmail.com>
2014-08-27 22:24:51 +0200

    added another learning mode during online training

--- src/main/java/net/snurkabill/neuralnetworks/feedforwardnetwork/FeedForwardNetworkOnlineManager.java
+++ src/main/java/net/snurkabill/neuralnetworks/feedforwardnetwork/FeedForwardNetworkOnlineManager.java
@@ -63,13 +63,14 @@ public class FeedForwardNetworkOnlineManager extends FeedForwardNetworkManager {
 		} else if(trainingMode == TrainingMode.PRE_TRAINING) {
 			LOGGER.debug("PRE_TAINING {}", expectedOutputClassIndex);
 			if(actualBatch == MAX_PRETRAINING_BATCH) {
-				trainingMode = TrainingMode.SEQUENTIAL_TRAINING;
+				trainingMode = TrainingMode.FULL_TRAINING;
 				LOGGER.info("PRE_TRAINING mode stopped. {} is on", trainingMode);
 				for (int i = 0; i < networks.size(); i++) {
 					for (int j = 0; j < networks.get(0).getSizeOfInputVector(); j++) {
 						networks.get(i).updateInputModifier(j, inputBatch[j]);
 					}
 				}
+				inputBatch = null;
 			} else {
 				for (int i = 0; i < inputVector.length; i++) {
 					inputBatch[i][actualBatch] = inputVector[i];
@@ -95,13 +96,16 @@ public class FeedForwardNetworkOnlineManager extends FeedForwardNetworkManager {
 	}
 	
 	public void changeTrainingMode(TrainingMode trainingMode) {
+		if(trainingMode == TrainingMode.PRE_TRAINING && inputBatch == null) {
+			LOGGER.info("OnlineManager already pretrained networks. It is not possible to pretrain them again");
+		}
 		LOGGER.debug("Changing training mode to {}", trainingMode);
 		this.trainingMode = trainingMode;
 	}
 	
 	public static enum TrainingMode {
 		FULL_TRAINING, // train on all inputs
-		SEQUENTIAL_TRAINING, // train sequential on classes of division {1, 2, 3 ....}
+		SEQUENTIAL_TRAINING, // train sequentially on classes of division {1, 2, 3 ....}
 		PRE_TRAINING, // store batch for pretraining neurons
 		NON_TRAINING; // back propagation off
 	}
