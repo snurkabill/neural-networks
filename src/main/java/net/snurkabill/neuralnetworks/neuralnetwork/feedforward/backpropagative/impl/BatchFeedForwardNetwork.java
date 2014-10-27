9cce777d44d71b83f9d2e70d1c349c1dc17ab5f6
Jiri Vahala <jvahala@redhat.com>
2014-10-27 13:06:19 +0100

    fixed minibatch training

--- src/main/java/net/snurkabill/neuralnetworks/neuralnetwork/feedforward/backpropagative/impl/BatchFeedForwardNetwork.java
+++ src/main/java/net/snurkabill/neuralnetworks/neuralnetwork/feedforward/backpropagative/impl/BatchFeedForwardNetwork.java
@@ -42,6 +42,7 @@ public class BatchFeedForwardNetwork extends BackPropagative {
     protected void weightsCalculation() {
         super.weightsCalculation();
         clearGradients();
+        trainedBeforeLearning = 0;
     }
 
     private void clearGradients() {
