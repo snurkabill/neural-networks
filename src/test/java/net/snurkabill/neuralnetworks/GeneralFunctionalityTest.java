b6b32035f2565cc67c06e9ee4b839ada6c32e80d
snurakbill <snurkabill@gmail.com>
2014-11-24 20:25:24 +0100

    some tests

--- src/test/java/net/snurkabill/neuralnetworks/GeneralFunctionalityTest.java
+++ src/test/java/net/snurkabill/neuralnetworks/GeneralFunctionalityTest.java
@@ -1,26 +1,31 @@
 package net.snurkabill.neuralnetworks;
 
-import net.snurkabill.neuralnetworks.data.mnist.MnistDatasetReader;
+import net.snurkabill.neuralnetworks.data.database.DataItem;
 import net.snurkabill.neuralnetworks.data.database.Database;
+import net.snurkabill.neuralnetworks.data.mnist.MnistDatasetReader;
 import net.snurkabill.neuralnetworks.heuristic.FFNNHeuristic;
 import net.snurkabill.neuralnetworks.heuristic.HeuristicDBN;
 import net.snurkabill.neuralnetworks.heuristic.HeuristicRBM;
 import net.snurkabill.neuralnetworks.managers.NetworkManager;
 import net.snurkabill.neuralnetworks.managers.boltzmannmodel.SupervisedRBMManager;
 import net.snurkabill.neuralnetworks.managers.boltzmannmodel.validator.PartialProbabilisticAssociationVectorValidator;
+import net.snurkabill.neuralnetworks.managers.deep.BinaryDeepBeliefNetworkManager;
+import net.snurkabill.neuralnetworks.managers.deep.StuckedRBMTrainer;
 import net.snurkabill.neuralnetworks.managers.feedforward.FeedForwardNetworkManager;
 import net.snurkabill.neuralnetworks.neuralnetwork.deep.BinaryDeepBeliefNetwork;
+import net.snurkabill.neuralnetworks.neuralnetwork.deep.StuckedRBM;
+import net.snurkabill.neuralnetworks.neuralnetwork.energybased.boltzmannmodel.restrictedboltzmannmachine.RestrictedBoltzmannMachine;
 import net.snurkabill.neuralnetworks.neuralnetwork.energybased.boltzmannmodel.restrictedboltzmannmachine.impl.BinaryRestrictedBoltzmannMachine;
 import net.snurkabill.neuralnetworks.neuralnetwork.feedforward.backpropagative.impl.online.OnlineFeedForwardNetwork;
 import net.snurkabill.neuralnetworks.neuralnetwork.feedforward.transferfunction.ParametrizedHyperbolicTangens;
 import net.snurkabill.neuralnetworks.weights.weightfactory.GaussianRndWeightsFactory;
 import net.snurkabill.neuralnetworks.weights.weightfactory.SmartGaussianRndWeightsFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -68,8 +73,100 @@ public class GeneralFunctionalityTest {
                 true, 63.0 == manager.getTestResults().getComparableSuccess());
     }
 
+    private List<DataItem> createNoise() {
+        List<DataItem> list = new ArrayList<>();
+        list.add(new DataItem(new double[]{0.0, 1.0}));
+        list.add(new DataItem(new double[]{1.0, 0.0}));
+        list.add(new DataItem(new double[]{0.0, 0.0}));
+        return list;
+    }
 
+    private List<DataItem> createAnd() {
+        List<DataItem> list = new ArrayList<>();
+        list.add(new DataItem(new double[]{1.0, 1.0}));
+        list.add(new DataItem(new double[]{1.0, 1.0}));
+        list.add(new DataItem(new double[]{1.0, 1.0}));
+        return list;
+    }
 
+    @Ignore
+    @Test
+    public void proofOfConcept() {
+        long seed = 0;
+        double weightsScale = 0.01;
+        Map<Integer, List<DataItem>> trainingSet = new HashMap<>();
+        Map<Integer, List<DataItem>> testingSet = new HashMap<>();
+
+        trainingSet.put(0, createAnd());
+        trainingSet.put(1, createNoise());
+
+        List<DataItem> and = new ArrayList<>();
+        List<DataItem> noise = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            and.addAll(createAnd());
+            noise.addAll(createNoise());
+        }
+        testingSet.put(0, and);
+        testingSet.put(1, noise);
+
+        Database database = new Database(0, trainingSet, testingSet, "Database");
+
+        HeuristicDBN heuristic = new HeuristicDBN();
+        heuristic.momentum = 0.1;
+        heuristic.learningRate = 0.1;
+        heuristic.constructiveDivergenceIndex = 1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 5;
+        heuristic.numofRunsBaseRBMItself = 1;
+        heuristic.numOfRunsOfNetworkChimney = 1;
+        List<RestrictedBoltzmannMachine> machines = new ArrayList<>(2);
+        machines.add(new BinaryRestrictedBoltzmannMachine("InnerRBM", database.getSizeOfVector(), 2,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, seed));
+        machines.add(new BinaryRestrictedBoltzmannMachine("InnerRBM2", 2, 2,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, seed));
+        StuckedRBM stuckedRBMs = new StuckedRBM(machines, "Stucked RBMs");
+        StuckedRBMTrainer.train(stuckedRBMs, database, 100, 100);
+        BinaryDeepBeliefNetwork DBN = new BinaryDeepBeliefNetwork("DBN", database.getNumberOfClasses(), 1,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, 0, stuckedRBMs);
+        NetworkManager manager = new BinaryDeepBeliefNetworkManager(DBN, database, null);
+        manager.supervisedTraining(100);
+        manager.testNetwork();
+        assertEquals("Result:" + manager.getTestResults().getComparableSuccess(),
+                0.0, manager.getTestResults().getComparableSuccess(), 0.00001);
+    }
+
+    @Ignore
+    @Test
+    public void learningBinaryDBN() {
+        long seed = 0;
+        double weightsScale = 0.01;
+        Database database = createDatabase(2000, true);
+        HeuristicDBN heuristic = new HeuristicDBN();
+        heuristic.momentum = 0.1;
+        heuristic.learningRate = 0.1;
+        heuristic.constructiveDivergenceIndex = 1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 5;
+        heuristic.numofRunsBaseRBMItself = 3;
+        heuristic.numOfRunsOfNetworkChimney = 3;
+        List<RestrictedBoltzmannMachine> machines = new ArrayList<>(2);
+        machines.add(new BinaryRestrictedBoltzmannMachine("InnerRBM", database.getSizeOfVector(), 300,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, seed));
+        machines.add(new BinaryRestrictedBoltzmannMachine("InnerRBM", 300, 200,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, seed));
+
+        StuckedRBM stuckedRBMs = new StuckedRBM(machines, "Stucked RBM");
+        StuckedRBMTrainer.train(stuckedRBMs, database, 10000, 10000);
+
+        BinaryDeepBeliefNetwork DBN = new BinaryDeepBeliefNetwork("DBN", database.getNumberOfClasses(), 1000,
+                new GaussianRndWeightsFactory(weightsScale, seed), heuristic, 0, stuckedRBMs);
+
+        NetworkManager manager = new BinaryDeepBeliefNetworkManager(DBN, database, null);
+        manager.supervisedTraining(10000);
+        manager.testNetwork();
+        assertEquals("Result:" + manager.getTestResults().getComparableSuccess(),
+                true, 63.0 == manager.getTestResults().getComparableSuccess());
+    }
 
 
     private Database createDatabase(int numOfElements, boolean binary) {
---
+++ src/test/java/net/snurkabill/neuralnetworks/scenario/energybased/RBMProofOfConceptTest.java
@@ -0,0 +1,107 @@
+package net.snurkabill.neuralnetworks.scenario.energybased;
+
+import net.snurkabill.neuralnetworks.data.database.DataItem;
+import net.snurkabill.neuralnetworks.data.database.Database;
+import net.snurkabill.neuralnetworks.heuristic.HeuristicRBM;
+import net.snurkabill.neuralnetworks.managers.NetworkManager;
+import net.snurkabill.neuralnetworks.managers.boltzmannmodel.UnsupervisedRBMManager;
+import net.snurkabill.neuralnetworks.managers.boltzmannmodel.validator.ProbabilisticAssociationVectorValidator;
+import net.snurkabill.neuralnetworks.neuralnetwork.energybased.boltzmannmodel.restrictedboltzmannmachine.impl.BinaryRestrictedBoltzmannMachine;
+import net.snurkabill.neuralnetworks.scenario.LogicFunctions;
+import net.snurkabill.neuralnetworks.weights.weightfactory.GaussianRndWeightsFactory;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RBMProofOfConceptTest extends LogicFunctions {
+
+    @Ignore
+    @Test
+    public void and() {
+        long seed = 0;
+        Map<Integer, List<DataItem>> trainingSet = new HashMap<>();
+        Map<Integer, List<DataItem>> testingSet = new HashMap<>();
+
+        List<DataItem> list = new ArrayList<>();
+        list.add(new DataItem(new double[]{1.0, 1.0}));
+        list.add(new DataItem(new double[]{0.0, 0.0}));
+        list.add(new DataItem(new double[]{1.0, 0.0}));
+        list.add(new DataItem(new double[]{0.0, 1.0}));
+
+        trainingSet.put(0, list);
+
+        List<DataItem> and = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            and.add(new DataItem(new double[]{1.0, 1.0}));
+            and.add(new DataItem(new double[]{0.0, 0.0}));
+            and.add(new DataItem(new double[]{1.0, 0.0}));
+            and.add(new DataItem(new double[]{0.0, 1.0}));
+        }
+        testingSet.put(0, and);
+
+        Database database = new Database(0, trainingSet, testingSet, "Database");
+
+        List<NetworkManager> managers = new ArrayList<>();
+
+        HeuristicRBM heuristic = new HeuristicRBM();
+        heuristic.learningRate = 0.1;
+        heuristic.momentum = 0.1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 1;
+        heuristic.constructiveDivergenceIndex = 1;
+        BinaryRestrictedBoltzmannMachine machine = new BinaryRestrictedBoltzmannMachine("RBM1",
+                database.getSizeOfVector(), 10,
+                new GaussianRndWeightsFactory(0.01, seed), heuristic, seed);
+        managers.add(new UnsupervisedRBMManager(machine, database, 0, null,
+                new ProbabilisticAssociationVectorValidator(10)));
+
+        heuristic = new HeuristicRBM();
+        heuristic.learningRate = 0.1;
+        heuristic.momentum = 0.1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 3;
+        heuristic.constructiveDivergenceIndex = 1;
+        machine = new BinaryRestrictedBoltzmannMachine("RBM2", database.getSizeOfVector(), 10,
+                new GaussianRndWeightsFactory(0.01, seed), heuristic, seed);
+        managers.add(new UnsupervisedRBMManager(machine, database, 0, null,
+                new ProbabilisticAssociationVectorValidator(10)));
+
+        heuristic = new HeuristicRBM();
+        heuristic.learningRate = 0.1;
+        heuristic.momentum = 0.1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 10;
+        heuristic.constructiveDivergenceIndex = 1;
+        machine = new BinaryRestrictedBoltzmannMachine("RBM3", database.getSizeOfVector(), 10,
+                new GaussianRndWeightsFactory(0.01, seed), heuristic, seed);
+        managers.add(new UnsupervisedRBMManager(machine, database, 0, null,
+                new ProbabilisticAssociationVectorValidator(10)));
+
+        heuristic = new HeuristicRBM();
+        heuristic.learningRate = 0.1;
+        heuristic.momentum = 0.1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 50;
+        heuristic.constructiveDivergenceIndex = 1;
+        machine = new BinaryRestrictedBoltzmannMachine("RBM4", database.getSizeOfVector(), 10,
+                new GaussianRndWeightsFactory(0.01, seed), heuristic, seed);
+        managers.add(new UnsupervisedRBMManager(machine, database, 0, null,
+                new ProbabilisticAssociationVectorValidator(10)));
+
+        heuristic = new HeuristicRBM();
+        heuristic.learningRate = 0.1;
+        heuristic.momentum = 0.1;
+        heuristic.temperature = 1;
+        heuristic.numOfTrainingIterations = 100;
+        heuristic.constructiveDivergenceIndex = 1;
+        machine = new BinaryRestrictedBoltzmannMachine("RBM5", database.getSizeOfVector(), 10,
+                new GaussianRndWeightsFactory(0.01, seed), heuristic, seed);
+        managers.add(new UnsupervisedRBMManager(machine, database, 0, null,
+                new ProbabilisticAssociationVectorValidator(10)));
+
+    }
+}
