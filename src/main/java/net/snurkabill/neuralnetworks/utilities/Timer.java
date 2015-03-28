92b5a21029cc45592bcb03ab4d33b1fd7f11390b
snurakbill <snurkabill@gmail.com>
2015-03-28 02:49:10 +0100

    Timer: timer now stops whenever is asked to get time

--- src/main/java/net/snurkabill/neuralnetworks/utilities/Timer.java
+++ src/main/java/net/snurkabill/neuralnetworks/utilities/Timer.java
@@ -4,24 +4,37 @@ public class Timer {
 
     private long startingTime;
     private long totalTime;
+    private boolean isRunning;
+
+    public boolean isRunning() {
+        return isRunning;
+    }
 
     public void startTimer() {
         startingTime = System.currentTimeMillis();
+        isRunning = true;
     }
 
     public void stopTimer() {
         totalTime = System.currentTimeMillis() - startingTime;
+        isRunning = false;
     }
 
     public long getTotalTime() {
+        if(isRunning) {
+            stopTimer();
+        }
         return totalTime;
     }
 
     public long secondsSpent() {
+        if(isRunning) {
+            stopTimer();
+        }
         return totalTime / 1000;
     }
 
     public double samplesPerSec(int samples) {
-        return (double) samples / (totalTime / 1000.0);
+        return (double) samples / (getTotalTime() / 1000.0);
     }
 }
