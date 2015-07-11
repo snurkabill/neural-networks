346542b1ba286095d0532486cf3dc158d5a87d44
Jiří Vahala <snurkabill@gmail.com>
2015-07-12 00:12:59 +0200

    Database: created classification database

---
+++ src/main/java/net/snurkabill/neuralnetworks/newdata/database/ClassificationDatabase.java
@@ -0,0 +1,283 @@
+package net.snurkabill.neuralnetworks.newdata.database;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Enumeration;
+
+public class ClassificationDatabase extends NewDatabase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassificationDatabase.class.getName());
+
+    private final NewDataItem[][] trainingSet;
+    private final NewDataItem[][] validationSet;
+    private final NewDataItem[][] testingSet;
+
+    public ClassificationDatabase(NewDataItem[][] trainingSet, NewDataItem[][] validationSet,
+                                  NewDataItem[][] testingSet, String databaseName, long seed) {
+        super(calculateSetSize(trainingSet), calculateSetSize(validationSet), calculateSetSize(testingSet),
+                trainingSet[0][0], databaseName, seed);
+        if(trainingSet[0][0].getTargetSize() != trainingSet.length) {
+            throw new IllegalArgumentException("Size of target vector doesn't correspond with count" +
+                    "of classes");
+        }
+        this.trainingSet = trainingSet;
+        this.validationSet = validationSet;
+        this.testingSet = testingSet;
+        LOGGER.info("Database created.");
+    }
+
+    private static int calculateSetSize(NewDataItem[][] set) {
+        int count = 0;
+        for (NewDataItem[] aSet : set) {
+            count += aSet.length;
+        }
+        return count;
+    }
+
+    public int getSizeOfTrainSetClass(int i) {
+        return trainingSet[i].length;
+    }
+
+    public int getSizeOfValidationSetClass(int i) {
+        return validationSet[i].length;
+    }
+
+    public int getSizeOfTestSetClass(int i) {
+        return testingSet[i].length;
+    }
+
+    @Override
+    public Enumeration<NewDataItem> createTrainingSetEnumerator() {
+        return new EpochSimpleTrainingEnumerator();
+    }
+
+    @Override
+    public Enumeration<NewDataItem> createInfiniteSimpleTrainingSetEnumerator() {
+        return new InfiniteSimpleTrainingEnumerator();
+    }
+
+    @Override
+    public Enumeration<NewDataItem> createInfiniteRandomTrainingSetEnumerator() {
+        return new InfiniteRandomTrainingEnumerator();
+    }
+
+    @Override
+    public Enumeration<NewDataItem> createValidationSetEnumerator() {
+        return new ValidationSetEnumerator();
+    }
+
+    @Override
+    public Enumeration<NewDataItem> createTestingSetEnumerator() {
+        return new TestingSetEnumerator();
+    }
+
+    public Enumeration<NewDataItem> createTestClassEnumerator(int _class) {
+        return new TestClassEnumerator(_class);
+    }
+
+    public Enumeration<NewDataItem> createTrainingClassEnumerator(int _class) {
+        return new TrainingClassEnumerator(_class);
+    }
+
+    public class InfiniteRandomTrainingEnumerator implements Enumeration<NewDataItem> {
+
+        @Override
+        public boolean hasMoreElements() {
+            return true;
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            int randomIndex = random.nextInt(trainingSetSize);
+            int tmpIndex = 0;
+            int previousTmpIndex = 0;
+            for (int i = 0; i < targetSize; i++) {
+                tmpIndex += trainingSet[i].length;
+                if(randomIndex < tmpIndex) {
+                    randomIndex -= previousTmpIndex;
+                    return trainingSet[i][randomIndex];
+                }
+                previousTmpIndex = tmpIndex;
+            }
+            throw new IllegalStateException("Index should have been picked");
+        }
+    }
+
+    public abstract class SimpleTrainingEnumerator implements Enumeration<NewDataItem> {
+        private final int[] lastUnused = new int[targetSize];
+        private int lastUnusedClass;
+        private int lastUsedClass;
+
+        public SimpleTrainingEnumerator() {
+            lastUnusedClass = 0;
+            for (int i = 0; i < lastUnused.length; i++) {
+                lastUnused[i] = trainingSet[i].length - 1;
+            }
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            lastUnusedClass = (lastUnusedClass + 1) % targetSize;
+            if (lastUnusedClass == 0) {
+                lastUsedClass = trainingSet.length - 1;
+            } else {
+                lastUsedClass = lastUnusedClass - 1;
+            }
+            lastUnused[lastUsedClass] = (lastUnused[lastUsedClass] + 1) % trainingSet[lastUsedClass].length;
+            if (lastUnused[lastUsedClass] == trainingSet[lastUsedClass].length) {
+                lastUnused[lastUsedClass] = 0;
+            }
+            return trainingSet[lastUsedClass][lastUnused[lastUsedClass]];
+        }
+
+        public int nextClass() {
+            return lastUnusedClass;
+        }
+
+        public int previousClass() {
+            return lastUsedClass;
+        }
+    }
+
+    public class InfiniteSimpleTrainingEnumerator extends SimpleTrainingEnumerator {
+
+        @Override
+        public boolean hasMoreElements() {
+            return true;
+        }
+    }
+
+    public class EpochSimpleTrainingEnumerator extends SimpleTrainingEnumerator {
+
+        private int counter = 0;
+
+        @Override
+        public boolean hasMoreElements() {
+            return counter < trainingSetSize;
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            NewDataItem item = super.nextElement();
+            counter++;
+            return item;
+        }
+    }
+
+    public abstract class ClassEnumerator implements Enumeration<NewDataItem> {
+
+        protected final int _class;
+        protected int index;
+
+        public ClassEnumerator(int _class) {
+            this._class = _class;
+            this.index = 0;
+        }
+    }
+
+    public class TrainingClassEnumerator extends ClassEnumerator {
+
+        public TrainingClassEnumerator(int _class) {
+            super(_class);
+        }
+
+        @Override
+        public boolean hasMoreElements() {
+            return index < trainingSet[_class].length;
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            index++;
+            return trainingSet[_class][index - 1];
+        }
+    }
+
+    public class TestClassEnumerator extends ClassEnumerator {
+
+        public TestClassEnumerator(int _class) {
+            super(_class);
+        }
+
+        @Override
+        public boolean hasMoreElements() {
+            return index < testingSet[_class].length;
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            index++;
+            return testingSet[_class][index - 1];
+        }
+    }
+
+    public class TestingSetEnumerator implements Enumeration<NewDataItem> {
+
+        private int _class;
+        private int index;
+
+        public TestingSetEnumerator() {
+            this._class = 0;
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasMoreElements() {
+            if(_class < targetSize - 1) {
+                return true;
+            } else {
+                return index < testingSet[_class].length;
+            }
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            index++;
+            NewDataItem item = testingSet[_class][index - 1];
+
+            if(!(index < testingSet[_class].length)) {
+                _class++;
+                if(_class < testingSet.length){
+                    index = 0;
+                }
+            }
+            return item;
+        }
+    }
+
+    public class ValidationSetEnumerator implements Enumeration<NewDataItem> {
+
+        private int _class;
+        private int index;
+
+        public ValidationSetEnumerator() {
+            this._class = 0;
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasMoreElements() {
+            if(_class < targetSize - 1) {
+                return true;
+            } else {
+                return index < validationSet[_class].length;
+            }
+        }
+
+        @Override
+        public NewDataItem nextElement() {
+            index++;
+            NewDataItem item = validationSet[_class][index - 1];
+
+            if(!(index < validationSet[_class].length)) {
+                _class++;
+                if(_class < validationSet.length){
+                    index = 0;
+                }
+            }
+            return item;
+        }
+    }
+
+}
---
+++ src/test/java/net/snurkabill/neuralnetworks/newdata/database/ClassificationDatabaseTest.java
@@ -0,0 +1,254 @@
+package net.snurkabill.neuralnetworks.newdata.database;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ClassificationDatabaseTest {
+
+    private static final int VECTOR_SIZE = 1;
+
+    private NewDataItem[][] generator(int numOfClasses, int[] numOfElements, int startingPoint) {
+        NewDataItem[][] data = new NewDataItem[numOfClasses][];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new NewDataItem[numOfElements[i]];
+        }
+        int helpNum = startingPoint;
+        for (int i = 0; i < numOfClasses; i++) {
+            for (int j = 0; j < numOfElements[i]; j++) {
+                double[] array = new double[VECTOR_SIZE];
+                for (int k = 0; k < VECTOR_SIZE; k++) {
+                    array[k] = helpNum;
+                    helpNum++;
+                }
+                double[] array2 = new double[numOfClasses];
+                for (int k = 0; k < numOfClasses; k++) {
+                    array2[k] = helpNum;
+                }
+                data[i][j] = new NewDataItem(array, array2);
+            }
+        }
+        return data;
+    }
+
+    @Test
+    public void lenOfClasses() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(trainSizes[i], classificationDatabase.getSizeOfTrainSetClass(i));
+            assertEquals(validationSizes[i], classificationDatabase.getSizeOfValidationSetClass(i));
+            assertEquals(testingSizes[i], classificationDatabase.getSizeOfTestSetClass(i));
+        }
+    }
+
+    @Test
+    public void setSizes() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        assertEquals(Arrays.stream(trainSizes).sum(), classificationDatabase.getTrainingSetSize());
+        assertEquals(Arrays.stream(validationSizes).sum(), classificationDatabase.getValidationSetSize());
+        assertEquals(Arrays.stream(testingSizes).sum(), classificationDatabase.getTestingSetSize());
+    }
+
+    @Test
+    public void hasTarget() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        assertTrue(classificationDatabase.hasTarget());
+    }
+
+    @Test
+    public void targetSize() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        assertEquals(3, classificationDatabase.getTargetSize());
+    }
+
+    @Test
+    public void vectorSize() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        assertEquals(VECTOR_SIZE, classificationDatabase.getVectorSize());
+    }
+
+    @Test
+    public void validationSetEnumerator() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        Enumeration<NewDataItem> enumerator = classificationDatabase.createValidationSetEnumerator();
+
+        int counter = 10; // starting element
+        for (int i = 0; i < validationSizes.length; i++) {
+            for (int j = 0; j < validationSizes[i]; j++) {
+                NewDataItem item = enumerator.nextElement();
+                assertEquals(counter, item.data[0], 0.000_000_1);
+                counter++;
+            }
+        }
+    }
+
+    @Test
+    public void testingSetEnumerator() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        Enumeration<NewDataItem> enumerator = classificationDatabase.createTestingSetEnumerator();
+
+        int counter = 100; // starting element
+        for (int i = 0; i < testingSizes.length; i++) {
+            for (int j = 0; j < testingSizes[i]; j++) {
+                NewDataItem item = enumerator.nextElement();
+                assertEquals(counter, item.data[0], 0.000_000_1);
+                counter++;
+            }
+        }
+    }
+
+    @Test
+    public void testingSetEnumeratorOverClass() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+
+        int counter = 100; // starting element
+        for (int k = 0; k < testingSizes.length; k++) {
+            Enumeration<NewDataItem> enumerator = classificationDatabase.createTestClassEnumerator(k);
+            int i = 0;
+            for (; enumerator.hasMoreElements(); i++) {
+                NewDataItem item = enumerator.nextElement();
+                assertEquals(counter, item.data[0], 0.000_000_1);
+                counter++;
+            }
+            assertEquals(testingSizes[k], i);
+        }
+    }
+
+    @Test
+    public void trainingSetEnumeratorOverClass() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+
+        int counter = 0; // starting element
+        for (int k = 0; k < testingSizes.length; k++) {
+            Enumeration<NewDataItem> enumerator = classificationDatabase.createTrainingClassEnumerator(k);
+            int i = 0;
+            for (; enumerator.hasMoreElements(); i++) {
+                NewDataItem item = enumerator.nextElement();
+                assertEquals(counter, item.data[0], 0.000_000_1);
+                counter++;
+            }
+            assertEquals(trainSizes[k], i);
+        }
+    }
+
+    @Test
+    public void trainingInfiniteSimpleSetEnumerator() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        Enumeration<NewDataItem> enumerator = classificationDatabase.createInfiniteSimpleTrainingSetEnumerator();
+        NewDataItem item = enumerator.nextElement();
+        assertEquals(0, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(1, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(3, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(0, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(2, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(4, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(0, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(1, item.data[0], 0.000_000_1);
+        item = enumerator.nextElement();
+        assertEquals(5, item.data[0], 0.000_000_1);
+    }
+
+    @Test
+    public void trainingInfiniteRandomSetEnumerator() {
+        int[] trainSizes = new int[] {1, 2, 3};
+        int[] validationSizes = new int[] {4, 5, 6};
+        int[] testingSizes = new int[] {7, 8, 9};
+        NewDataItem[][] trainingSet = generator(3, trainSizes, 0);
+        NewDataItem[][] validationSet = generator(3, validationSizes, 10);
+        NewDataItem[][] testingSet = generator(3, testingSizes, 100);
+
+        ClassificationDatabase classificationDatabase = new ClassificationDatabase(trainingSet, validationSet, testingSet,
+                "name", 0);
+        Enumeration<NewDataItem> enumerator = classificationDatabase.createInfiniteRandomTrainingSetEnumerator();
+        for (int i = 0; i < 1000; i++) {
+            NewDataItem item = enumerator.nextElement();
+        }
+    }
+
+}
